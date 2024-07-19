fd8113564c5038d15d03a5d9dfa2ce1c46776831
Eddy Chan <yuenho12345@gmail.com>
2024-07-19 23:00:27 +0800

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import React from "react";
-import Navbar from "./components/Navbar";
+import Narbar from "./components/Narbar";
 
 function App() {
   return (
-    <main className="overflow-x-hidden bg-black tracking-tighter text-gray-200 antialiased">
-      <Navbar />
+    <main>
+      <Narbar />
     </main>
   );
 }
---
+++ src/components/Narbar.jsx
@@ -0,0 +1,19 @@
+function Narbar() {
+  return (
+    <nav className="bg-black text-white">
+      <div>
+        <div>Eddy</div>
+        <div>
+          <a href="#">Home</a>
+          <a href="#">About Me</a>
+          <a href="#">Service</a>
+          <a href="#">Projects</a>
+          <a href="#">Contact</a>
+        </div>
+        <button>Connect Me</button>
+      </div>
+    </nav>
+  );
+}
+
+export default Narbar;
