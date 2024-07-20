803ed97843f0df118c49e4eb560992da401fbc7b
Yuen Ho <98795653+eddy1129@users.noreply.github.com>
2024-07-20 17:27:05 +0800

    added skill sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 
 function App() {
   return (
-    <main className="overflow-x-hidden bg-black tracking-tighter text-gray-50 antialiased">
+    <main className="overflow-x-hidden bg-black tracking-tighter text-gray-200 antialiased">
       <Navbar />
       <HeroSection />
+      <About />
+      <Projects />
+      <Skills />
     </main>
   );
 }
